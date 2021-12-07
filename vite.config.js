d1cd6b5cce7a740c97965a288439c96804d520da
Nazim <nazimali@gmail.com>
2021-12-06 21:21:30 -0500

    INIT: Add release flow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { resolve } from "path";
 
 export default defineConfig({
+    build: {
+        lib: {
+            entry: "./src/utils/PrayerTimes.ts",
+            name: "BeautifulSalat",
+            fileName: (format) => `beautiful-salat.${format}.js`,
+        },
+    },
     plugins: [svelte()],
     resolve: {
         alias: {
