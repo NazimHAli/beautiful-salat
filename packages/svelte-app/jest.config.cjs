9f4b8793001f154ffce4e1406b04d43ea05a5959
Nazim <nazimali@gmail.com>
2021-12-12 16:59:12 -0500

    style: uncomment jest config

--- packages/svelte-app/jest.config.cjs
+++ packages/svelte-app/jest.config.cjs
@@ -8,40 +8,18 @@ module.exports = {
         prefix: "<rootDir>/",
     }),
     moduleDirectories: ["node_modules", "src"],
+    collectCoverage: true,
+    collectCoverageFrom: ["<rootDir>/src/utils/*.ts", "!<rootDir>/src/*.d.ts"],
+    coverageReporters: ["text", "text-summary"],
+    coverageThreshold: {
+        global: {
+            branches: 10,
+            functions: 10,
+            lines: 10,
+            statements: 10,
+        },
+    },
+    restoreMocks: true,
+    resetMocks: true,
+    resetModules: true,
 };
-
-// "jest": {
-//   "collectCoverage": true,
-//   "collectCoverageFrom": [
-//       "<rootDir>/src/utils/*.ts",
-//       "!<rootDir>/src/*.d.ts"
-//   ],
-//   "coverageReporters": [
-//       "text",
-//       "text-summary"
-//   ],
-//   "coverageThreshold": {
-//       "global": {
-//           "branches": 10,
-//           "functions": 10,
-//           "lines": 10,
-//           "statements": 10
-//       }
-//   },
-//   "moduleDirectories": [
-//       "node_modules",
-//       "src"
-//   ],
-//   "modulePaths": [
-//       "<rootDir>/src"
-//   ],
-//   "moduleNameMapper": {
-//       "^@/(.*)$": "<rootDir>/src/$1"
-//   },
-//   "transform": {
-//       "^.+\\.(ts|js)$": "ts-jest"
-//   },
-//   "restoreMocks": true,
-//   "resetMocks": true,
-//   "resetModules": true
-// },
