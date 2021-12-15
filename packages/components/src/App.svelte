190ae36548397dadc92a843cf7100384a98d0764
Nazim <nazimali@gmail.com>
2021-12-15 01:14:57 -0500

    fix: fix example build bug

--- packages/components/src/App.svelte
+++ packages/components/src/App.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-  import { PrayerTimes } from "beautiful-salat";
+  import { PrayerTimes } from "@beautiful-salat/js-lib";
   import PrayerTimesMonthly from "@/components/PrayerTimesMonthly.svelte";
-  import PrayerTimesTableDaily from "@/components/PrayerTimesTableDaily.svelte";
+  import PrayerTimesDaily from "@/components/PrayerTimesDaily.svelte";
 
   const ptModule = new PrayerTimes("ISNA");
   const prayerTimes = ptModule.getPrayerTimes(new Date(), [43, -80], -5);
+  const monthly = ptModule.getMonthTable([43, -80], 0, -5);
 </script>
 
-<PrayerTimesTableDaily prayerTimes={Object.entries(prayerTimes)} />
+<PrayerTimesDaily prayerTimes={Object.entries(prayerTimes)} />
 
-<PrayerTimesMonthly theme="classic" />
+<PrayerTimesMonthly theme="dark" calculatedPrayerTimes={monthly} />
