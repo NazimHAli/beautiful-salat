1ae670d005775c881c609a12d8814fd83ef9520c
Nazim <nazimali@gmail.com>
2021-12-15 08:07:56 -0500

    refactor: daily component uses the same theme styling

--- packages/components/src/components/PrayerTimesDaily.svelte
+++ packages/components/src/components/PrayerTimesDaily.svelte
@@ -1,18 +1,21 @@
 <script lang="ts">
   export let prayerTimes = [];
+  export let theme = "dark";
 
   let displayColumns = ["Fajr", "Dhuhr", "Asr", "Maghrib", "Isha"];
 </script>
 
 <div class="table-container">
-  <table class="table">
-    <thead class="table-header">
-      <th>Prayer Times</th>
+  <table class={theme + "_table"}>
+    <thead>
+      <tr class={theme + "_title"}>
+        <th class={theme + "_title_cell"}>Prayer Times</th>
+      </tr>
     </thead>
 
-    <tbody class="table-body">
+    <tbody class={theme + "_body"}>
       {#each displayColumns as name}
-        <tr>
+        <tr class={theme + "_row"}>
           <th>{name}</th>
           <td>{prayerTimes[name]}</td>
         </tr>
@@ -22,5 +25,5 @@
 </div>
 
 <style lang="scss">
-  @import "../styles/card.scss";
+  @import "../styles/themes.scss";
 </style>
