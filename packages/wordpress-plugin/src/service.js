19bde6dd6f892a222b62907ce8eda2eb59f46414
Nazim <nazimali@gmail.com>
2021-12-28 01:14:53 -0500

    refactor: extract service call, formatting, rendering

---
+++ packages/wordpress-plugin/src/service.js
@@ -0,0 +1,18 @@
+function to12hourFormat(dateStr) {
+  return new Date(dateStr).toLocaleTimeString().toUpperCase();
+}
+
+export async function getSalatTimes(args) {
+  const API = "https://api.aladhan.com/v1/timingsByCity";
+  const API_EXAMPLE = `${API}?city=${args.city}&country=${args.country}&iso8601=true`;
+
+  const rawResponse = await fetch(API_EXAMPLE);
+  const responseJson = await rawResponse.json();
+  const response = responseJson.data;
+
+  for (const salat in response?.timings) {
+    response.timings[salat] = to12hourFormat(response.timings[salat]);
+  }
+
+  return response;
+}
