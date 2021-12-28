12aac3977010e296bbcf76dd8806fcd249b7353d
Nazim <nazimali@gmail.com>
2021-12-28 01:41:31 -0500

    refactor: check timinings exist

--- packages/wordpress-plugin/src/service.js
+++ packages/wordpress-plugin/src/service.js
@@ -6,15 +6,20 @@ async function getSalatTimes(args) {
   const API = "https://api.aladhan.com/v1/timingsByCity";
   const API_EXAMPLE = `${API}?city=${args.city}&country=${args.country}&iso8601=true`;
 
-  const rawResponse = await fetch(API_EXAMPLE);
-  const responseJson = await rawResponse.json();
-  const response = responseJson.data;
+  try {
+    const rawResponse = await fetch(API_EXAMPLE);
+    const responseJson = await rawResponse.json();
+    const response = responseJson.data;
 
-  for (const salat in response?.timings) {
-    response.timings[salat] = dateTo12hourFormat(response.timings[salat]);
+    for (const salat in response?.timings) {
+      response.timings[salat] = dateTo12hourFormat(response.timings[salat]);
+    }
+
+    return response;
+  } catch (error) {
+    console.log(error);
+    return {};
   }
-
-  return response;
 }
 
 export { getSalatTimes };
