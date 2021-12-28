df49afe32fe19a3f28bbefee5411af2644f1a25f
Nazim <nazimali@gmail.com>
2021-12-28 01:14:53 -0500

    refactor: extract service call, formatting, rendering

--- packages/wordpress-plugin/src/edit.js
+++ packages/wordpress-plugin/src/edit.js
@@ -4,47 +4,55 @@ import { __ } from "@wordpress/i18n";
 
 import { prayerTable } from "./prayerTable";
 import { prayerMethods } from "./prayerMethods";
+import { getSalatTimes } from "./service";
 
-async function getSalatTimes(args) {
-  const API = "https://api.aladhan.com/v1/timingsByCity";
-  const API_EXAMPLE = `${API}?city=${args.city}&country=${args.country}`;
-  const res = await fetch(API_EXAMPLE);
-  const resjson = await res.json();
-  console.log(resjson.data);
-}
-
-const BackEndEdit = (props) => {
+function BackEndEdit(props) {
   const blockProps = useBlockProps();
   const { setAttributes } = props;
 
-  const onChangeToggleField = (newValue) => {
-    setAttributes({ toggleField: newValue });
+  const onChangeShowHeader = (newValue) => {
+    setAttributes({ showHeader: newValue });
   };
 
   const onCountryChange = (newValue) => {
-    setAttributes({ salatSettings: { ...props.attributes.salatSettings, country: newValue } });
-    getSalatTimes(props.attributes.salatSettings);
+    getSalatTimes({ ...props.attributes.salatSettings, country: newValue }).then((response) => {
+      if (response) {
+        setAttributes({
+          salatSettings: {
+            ...props.attributes.salatSettings,
+            country: newValue,
+            method: response?.meta?.method?.id,
+            timings: response?.timings,
+          },
+        });
+      }
+    });
   };
 
   const onCityChange = (newValue) => {
     setAttributes({ salatSettings: { ...props.attributes.salatSettings, city: newValue } });
   };
 
+  const onMethodChange = (newValue) => {
+    setAttributes({ salatSettings: { ...props.attributes.salatSettings, method: newValue } });
+  };
+
   return (
     <div {...blockProps}>
       <InspectorControls>
-        <PanelBody title={__("Salat Settings")}>
+        <PanelBody title={__("Salat Settings")} initialOpen={false}>
           <SelectControl
-            label="Method"
+            label="Selected Method"
+            help="Based on country and city"
             value={props.attributes.salatSettings.method}
             options={prayerMethods}
-            onChange={(newValue, oldValue) => setAttributes({ ...oldValue, method: newValue })}
+            onChange={onMethodChange}
           />
           <TextControl label="Country" onChange={onCountryChange} value={props.attributes.salatSettings.country} />
           <TextControl label="City" onChange={onCityChange} value={props.attributes.salatSettings.city} />
         </PanelBody>
 
-        <PanelBody title={__("Header")}>
+        <PanelBody title={__("Header")} initialOpen={false}>
           <TextControl label="Title" onChange={(newValue) => setAttributes({ title: newValue })} value={props.attributes.title} />
 
           <p>Title Text Color</p>
@@ -52,10 +60,10 @@ const BackEndEdit = (props) => {
 
           <p>Title Background Color</p>
           <ColorPalette value={props.attributes.backgroundColor} onChange={(newValue) => setAttributes({ backgroundColor: newValue })} />
-          <ToggleControl label="Toggle Field" checked={props.attributes.toggleField} onChange={onChangeToggleField} />
+          <ToggleControl label="Toggle Header" checked={props.attributes.showHeader} onChange={onChangeShowHeader} />
         </PanelBody>
 
-        <PanelBody title={__("Container")}>
+        <PanelBody title={__("Container")} initialOpen={false}>
           <TextControl label="Title" onChange={(newValue) => setAttributes({ title: newValue })} value={props.attributes.title} />
         </PanelBody>
       </InspectorControls>
@@ -63,6 +71,6 @@ const BackEndEdit = (props) => {
       {prayerTable(props.attributes)}
     </div>
   );
-};
+}
 
 export { BackEndEdit };
--- packages/wordpress-plugin/src/prayerTable.js
+++ packages/wordpress-plugin/src/prayerTable.js
@@ -1,43 +1,47 @@
 export function prayerTable(props) {
+  const timings = props.salatSettings.timings;
+
   return (
     <div style={{ maxWidth: props?.maxWidth ? props.maxWidth : null }} className="table-container">
       <table className="table">
-        <thead
-          style={{
-            backgroundColor: props?.backgroundColor ? props.backgroundColor : null,
-            color: props?.titleTextColor ? props.titleTextColor : null,
-          }}
-          className="table-header"
-        >
-          <tr>
-            <th>{props?.title}</th>
-          </tr>
-        </thead>
+        {props.showHeader && (
+          <thead
+            style={{
+              backgroundColor: props?.backgroundColor ? props.backgroundColor : null,
+              color: props?.titleTextColor ? props.titleTextColor : null,
+            }}
+            className="table-header"
+          >
+            <tr>
+              <th>{props?.title}</th>
+            </tr>
+          </thead>
+        )}
 
         <tbody className="table-body">
           <tr>
             <th>Fajr:</th>
-            <td>05:00</td>
+            <td>{timings["Fajr"]}</td>
           </tr>
 
           <tr>
-            <th>Duhr:</th>
-            <td>12:00</td>
+            <th>Dhuhr:</th>
+            <td>{timings["Dhuhr"]}</td>
           </tr>
 
           <tr>
             <th>Asr:</th>
-            <td>04:00</td>
+            <td>{timings["Asr"]}</td>
           </tr>
 
           <tr>
             <th>Maghrib:</th>
-            <td>05:30</td>
+            <td>{timings["Maghrib"]}</td>
           </tr>
 
           <tr>
             <th>Isha:</th>
-            <td>06:45</td>
+            <td>{timings["Isha"]}</td>
           </tr>
         </tbody>
       </table>
