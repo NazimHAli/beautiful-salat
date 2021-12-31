d8bfc38cffb9930fd1f49bb423979848815338af
Nazim <nazimali@gmail.com>
2021-12-31 16:33:48 -0500

    refactor: brekup backend method into smaller methods, get initial prayer times if empty

--- packages/wordpress-plugin/src/admin/edit.js
+++ packages/wordpress-plugin/src/admin/edit.js
@@ -9,15 +9,91 @@ import {
   SelectControl,
   PanelBody,
 } from "@wordpress/components";
+import { useEffect } from "@wordpress/element";
 import { __ } from "@wordpress/i18n";
 
 import { prayerTable } from "../frontend/prayerTable";
 import { prayerMethods } from "./prayerMethods";
 import { getSalatTimes } from "./service";
 
+function panelSalatSettings(
+  props,
+  onMethodChange,
+  onCountryChange,
+  onCityChange
+) {
+  return (
+    <PanelBody title={__("Salat Settings")} initialOpen={false}>
+      <SelectControl
+        label="Calculation Method"
+        help="Based on country and city"
+        value={props.attributes.salatSettings.method}
+        options={prayerMethods}
+        onChange={onMethodChange}
+      />
+      <TextControl
+        label="Country Code"
+        onChange={onCountryChange}
+        value={props.attributes.salatSettings.country}
+      />
+      <TextControl
+        label="City"
+        onChange={onCityChange}
+        value={props.attributes.salatSettings.city}
+      />
+    </PanelBody>
+  );
+}
+
+function panelHeaderSettings(props, onChangeShowHeader, setAttributes) {
+  return (
+    <PanelBody title={__("Header")} initialOpen={false}>
+      <ToggleControl
+        label="Toggle Header"
+        checked={props.attributes.showHeader}
+        onChange={onChangeShowHeader}
+      />
+
+      <TextControl
+        label="Title"
+        onChange={(newValue) => setAttributes({ title: newValue })}
+        value={props.attributes.title}
+      />
+
+      <p>Title Text Color</p>
+      <ColorPalette
+        value={props.attributes.titleTextColor}
+        onChange={(newValue) => setAttributes({ titleTextColor: newValue })}
+      />
+
+      <p>Title Background Color</p>
+      <ColorPalette
+        value={props.attributes.backgroundColor}
+        onChange={(newValue) => setAttributes({ backgroundColor: newValue })}
+      />
+    </PanelBody>
+  );
+}
+
 function AdminBackEnd(props) {
   const blockProps = useBlockProps();
   const { setAttributes } = props;
+
+  useEffect(() => {
+    if (!props.attributes.salatSettings.timings?.Dhuhr) {
+      getSalatTimes(props.attributes.salatSettings).then((response) => {
+        if (response) {
+          setAttributes({
+            salatSettings: {
+              ...props.attributes.salatSettings,
+              method: response?.meta?.method?.id,
+              timings: response?.timings,
+            },
+          });
+        }
+      });
+    }
+  }, []);
 
   const onChangeShowHeader = (newValue) => {
     setAttributes({ showHeader: newValue });
@@ -56,53 +132,14 @@ function AdminBackEnd(props) {
   return (
     <div {...blockProps}>
       <InspectorControls>
-        <PanelBody title={__("Salat Settings")} initialOpen={false}>
-          <SelectControl
-            label="Calculation Method"
-            help="Based on country and city"
-            value={props.attributes.salatSettings.method}
-            options={prayerMethods}
-            onChange={onMethodChange}
-          />
-          <TextControl
-            label="Country"
-            onChange={onCountryChange}
-            value={props.attributes.salatSettings.country}
-          />
-          <TextControl
-            label="City"
-            onChange={onCityChange}
-            value={props.attributes.salatSettings.city}
-          />
-        </PanelBody>
+        {panelSalatSettings(
+          props,
+          onMethodChange,
+          onCountryChange,
+          onCityChange
+        )}
 
-        <PanelBody title={__("Header")} initialOpen={false}>
-          <ToggleControl
-            label="Toggle Header"
-            checked={props.attributes.showHeader}
-            onChange={onChangeShowHeader}
-          />
-
-          <TextControl
-            label="Title"
-            onChange={(newValue) => setAttributes({ title: newValue })}
-            value={props.attributes.title}
-          />
-
-          <p>Title Text Color</p>
-          <ColorPalette
-            value={props.attributes.titleTextColor}
-            onChange={(newValue) => setAttributes({ titleTextColor: newValue })}
-          />
-
-          <p>Title Background Color</p>
-          <ColorPalette
-            value={props.attributes.backgroundColor}
-            onChange={(newValue) =>
-              setAttributes({ backgroundColor: newValue })
-            }
-          />
-        </PanelBody>
+        {panelHeaderSettings(props, onChangeShowHeader, setAttributes)}
 
         <PanelBody title={__("Container")} initialOpen={false}>
           <TextControl
