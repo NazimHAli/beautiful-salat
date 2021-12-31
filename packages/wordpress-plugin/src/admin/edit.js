5771bf8671ac25e446c11f94d592e72276bc5dc0
Nazim <nazimali@gmail.com>
2021-12-31 16:48:05 -0500

    refactor: move panels to their own file

--- packages/wordpress-plugin/src/admin/edit.js
+++ packages/wordpress-plugin/src/admin/edit.js
@@ -1,78 +1,27 @@
-import {
-  ColorPalette,
-  InspectorControls,
-  useBlockProps,
-} from "@wordpress/block-editor";
-import {
-  TextControl,
-  ToggleControl,
-  SelectControl,
-  PanelBody,
-} from "@wordpress/components";
+import { InspectorControls, useBlockProps } from "@wordpress/block-editor";
 import { useEffect } from "@wordpress/element";
-import { __ } from "@wordpress/i18n";
-
 import { prayerTable } from "../frontend/prayerTable";
-import { prayerMethods } from "./prayerMethods";
+import {
+  panelContainerSettings,
+  panelHeaderSettings,
+  panelSalatSettings,
+} from "./editPanels";
 import { getSalatTimes } from "./service";
 
-function panelSalatSettings(
-  props,
-  onMethodChange,
-  onCountryChange,
-  onCityChange
-) {
-  return (
-    <PanelBody title={__("Salat Settings")} initialOpen={false}>
-      <SelectControl
-        label="Calculation Method"
-        help="Based on country and city"
-        value={props.attributes.salatSettings.method}
-        options={prayerMethods}
-        onChange={onMethodChange}
-      />
-      <TextControl
-        label="Country Code"
-        onChange={onCountryChange}
-        value={props.attributes.salatSettings.country}
-      />
-      <TextControl
-        label="City"
-        onChange={onCityChange}
-        value={props.attributes.salatSettings.city}
-      />
-    </PanelBody>
-  );
-}
-
-function panelHeaderSettings(props, onChangeShowHeader, setAttributes) {
-  return (
-    <PanelBody title={__("Header")} initialOpen={false}>
-      <ToggleControl
-        label="Toggle Header"
-        checked={props.attributes.showHeader}
-        onChange={onChangeShowHeader}
-      />
-
-      <TextControl
-        label="Title"
-        onChange={(newValue) => setAttributes({ title: newValue })}
-        value={props.attributes.title}
-      />
-
-      <p>Title Text Color</p>
-      <ColorPalette
-        value={props.attributes.titleTextColor}
-        onChange={(newValue) => setAttributes({ titleTextColor: newValue })}
-      />
-
-      <p>Title Background Color</p>
-      <ColorPalette
-        value={props.attributes.backgroundColor}
-        onChange={(newValue) => setAttributes({ backgroundColor: newValue })}
-      />
-    </PanelBody>
-  );
+function setInitialPrayerTimes(props, setAttributes) {
+  if (!props.attributes.salatSettings.timings?.Dhuhr) {
+    getSalatTimes(props.attributes.salatSettings).then((response) => {
+      if (response) {
+        setAttributes({
+          salatSettings: {
+            ...props.attributes.salatSettings,
+            method: response?.meta?.method?.id,
+            timings: response?.timings,
+          },
+        });
+      }
+    });
+  }
 }
 
 function AdminBackEnd(props) {
@@ -80,19 +29,7 @@ function AdminBackEnd(props) {
   const { setAttributes } = props;
 
   useEffect(() => {
-    if (!props.attributes.salatSettings.timings?.Dhuhr) {
-      getSalatTimes(props.attributes.salatSettings).then((response) => {
-        if (response) {
-          setAttributes({
-            salatSettings: {
-              ...props.attributes.salatSettings,
-              method: response?.meta?.method?.id,
-              timings: response?.timings,
-            },
-          });
-        }
-      });
-    }
+    setInitialPrayerTimes(props, setAttributes);
   }, []);
 
   const onChangeShowHeader = (newValue) => {
@@ -141,13 +78,7 @@ function AdminBackEnd(props) {
 
         {panelHeaderSettings(props, onChangeShowHeader, setAttributes)}
 
-        <PanelBody title={__("Container")} initialOpen={false}>
-          <TextControl
-            label="Title"
-            onChange={(newValue) => setAttributes({ title: newValue })}
-            value={props.attributes.title}
-          />
-        </PanelBody>
+        {panelContainerSettings(setAttributes, props)}
       </InspectorControls>
 
       {prayerTable(props.attributes)}
