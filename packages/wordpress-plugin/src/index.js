4f1ad0c90f0ce4f94ecd061e38f4c6caea8bea02
Nazim <nazimali@gmail.com>
2022-01-01 09:20:35 -0500

    refactor: move save method to frontend index.js

--- packages/wordpress-plugin/src/index.js
+++ packages/wordpress-plugin/src/index.js
@@ -1,13 +1,10 @@
 import "./frontend/style.scss";
 
 import { registerBlockType } from "@wordpress/blocks";
-import { __ } from "@wordpress/i18n";
 import { AdminBackEnd } from "./admin/index";
-import { prayerTable } from "./frontend/prayerTable";
+import { ClientSideUI } from "./frontend/index";
 
 registerBlockType("fit/beautiful-salat", {
   edit: AdminBackEnd,
-  save: (props) => {
-    return <>{prayerTable(props.attributes)}</>;
-  },
+  save: ClientSideUI,
 });
