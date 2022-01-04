537ef3ada63061144dfc0782188e76089cf52b40
Nazim <nazimali@gmail.com>
2022-01-03 20:19:45 -0500

    style: add block icon for better visibility

--- packages/wordpress-plugin/src/index.js
+++ packages/wordpress-plugin/src/index.js
@@ -1,7 +1,9 @@
 import { registerBlockType } from "@wordpress/blocks";
 import { AdminBackEnd } from "./admin/index";
 import "./frontend/style.scss";
+import SalatMatIcon from "./admin/salatMatIcon";
 
 registerBlockType("fit/beautiful-salat", {
+  icon: <SalatMatIcon />,
   edit: AdminBackEnd,
 });
